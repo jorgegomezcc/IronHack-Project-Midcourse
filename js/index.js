65fc7dbca9a9c7f0b9af7ef2ebd0ba8d7f4c021b
Jorge Gomez <andispotlight@gmail.com>
2023-05-30 18:04:53 +0200

    commit 8 (13 taking into account work on other devices and repositories) changes in html, adding css changes

--- js/index.js
+++ js/index.js
@@ -7,17 +7,15 @@
 
 //Function to get API content, DOM manipulation and onload event
 const getApiContent = () => {
-    fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects")
+    fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
         .then((res) => {
             for (let i = 0; i < 3; i++) {
                 document.querySelector('#api-cards-container').innerHTML += `<div class="api-cards line-limit">
-                <img class="api-img" src="./images/projects-section/${res[i].uuid}.jpg" alt="Project ${res[i].uuid} img">
-                <h3>${res[i].name}</h3>
-                <h4>${res[i].description}</h4>
-                <p>${res[i].content}</p>
-                <p>${res[i].completed_on}</p>
-                <a href="project.html?p=${res[i].uuid}">Learn More</a>
+                <img class="api-img" src="./images/projects-section/${res[i].id}.jpg" alt="Project ${res[i].id} img">
+                <h3>${res[i].title}</h3>
+                <p>${res[i].body}</p>
+                <p><a id="learn-more" href="project.html?p=${res[i].id}">Learn More</a></p>
                 </div>`;
             }
         })
