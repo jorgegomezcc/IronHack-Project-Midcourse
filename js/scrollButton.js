e10f94179c49ea3891b0f346d87aca4af1fdf064
Jorge Gomez <andispotlight@gmail.com>
2023-06-02 00:14:04 +0200

    commit13 cleanin some code, loader js edited, contact style css finished, brands animation finished, pending about us

--- js/scrollButton.js
+++ js/scrollButton.js
@@ -39,9 +39,13 @@ const subscribeBtn = () => {
 // Function to loading effect
 const loadSpinner = () => {
     const loading = document.getElementById('lds-ring');
-    setTimeout(() => {
-      loading.style.display = 'none';    
-    }, 1800);
+    if (screen.width <= 990) {
+        loading.style.display = 'none';
+    }else {
+        setTimeout(() => {
+            loading.style.display = 'none';    
+          }, 1200);
+    }
   };
 
   //Add listener, when load the page call the function
